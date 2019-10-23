604377c66ce5993b3cb66dc055a74cabcae977d9
Dmitry Smetyukhov <dmitry.smetyukhov@gmail.com>
2019-10-23 20:49:06 +0300

    fix

--- src/app/shared/app-constants.ts
+++ src/app/shared/app-constants.ts
@@ -3,7 +3,7 @@ export class AppConstants {
 
   public static LOCAL_STORAGE_PREFIX = 'test_task_';
   public static GITHUB_MODULE_STATE_PREFIX = 'github_module';
-  public static GIT_SEARCH_LIMIT = 15;
+  public static GIT_SEARCH_LIMIT = 10;
   public static GIT_AVAILABLE_RESULTS_NUMBER = 1000;
   public static GIT_SUCCESS_MESSAGE = 'Success!';
 
